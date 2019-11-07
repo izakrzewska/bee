3d80b6fc0d01c1234913d6e7e7a2884e778b5db9
izakrzewska <pl.iga.zakrzewska@gmail.com>
2019-11-07 19:27:34 +0100

    rewrite apiaryDetails and BeehivesList to functional components

--- client/components/Apiaries/ApiaryDetails.js
+++ client/components/Apiaries/ApiaryDetails.js
@@ -1,36 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import { graphql } from "react-apollo";
 import fetchApiary from "../../queries/fetchApiary";
 import { Link } from "react-router";
 import BeehiveCreate from "../Beehives/BeehiveCreate";
 import BeehivesList from "../Beehives/BeehivesList";
 
-class ApiaryDetails extends Component {
-  render() {
-    const { apiary } = this.props.data;
+const ApiaryDetails = props => {
+  const { apiary } = props.data;
 
-    if (!apiary) {
-      return <div>Loading...</div>;
-    }
-    const { name, beehives, numberOfBeehivesInRow } = apiary;
-    const numberOfBeehives = beehives.length;
-
-    return (
-      <div>
-        <Link to='/'>Back</Link>
-        <h3>{name}</h3>
-        <div>{`Liczba uli w pasiece: ${numberOfBeehives}`}</div>
-        <div>{`Liczba uli w rzędzie: ${numberOfBeehivesInRow}`}</div>
-        <BeehivesList beehives={beehives} />
-        <BeehiveCreate
-          numberOfBeehives={numberOfBeehives}
-          numberOfBeehivesInRow={numberOfBeehivesInRow}
-          apiaryId={this.props.params.id}
-        />
-      </div>
-    );
+  if (!apiary) {
+    return <div>Loading...</div>;
   }
-}
+
+  const { name, beehives, numberOfBeehivesInRow } = apiary;
+  const numberOfBeehives = beehives.length;
+
+  return (
+    <div>
+      <Link to='/'>Back</Link>
+      <h3>{name}</h3>
+      <div>{`Liczba uli w pasiece: ${numberOfBeehives}`}</div>
+      <div>{`Liczba uli w rzędzie: ${numberOfBeehivesInRow}`}</div>
+      <BeehivesList beehives={beehives} />
+      <BeehiveCreate
+        numberOfBeehives={numberOfBeehives}
+        numberOfBeehivesInRow={numberOfBeehivesInRow}
+        apiaryId={props.params.id}
+      />
+    </div>
+  );
+};
 
 export default graphql(fetchApiary, {
   options: props => {
--- client/components/Beehives/BeehivesList.js
+++ client/components/Beehives/BeehivesList.js
@@ -1,30 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class BeehivesList extends Component {
-  renderBeehives() {
-    return this.props.beehives.map(
-      ({ content, colors, active, position, id }) => {
-        return (
-          <li key={id} className='collection-item'>
-            {content}
-            {colors.map(color => (
-              <div>{color}</div>
-            ))}
-            {active ? "Aktywny" : "Nieaktywny"}
-            <div>
-              {`Rząd: ${position.row} Miejsce w rzędzie: ${position.number}`}
-            </div>
-          </li>
-        );
-      }
-    );
-  }
+const BeehivesList = ({ beehives }) => {
+  const renderBeehives = beehives => {
+    return beehives.map(({ content, colors, active, position, id }) => {
+      return (
+        <li key={id} className='collection-item'>
+          {content}
+          {colors.map(color => (
+            <div key={color}>{color}</div>
+          ))}
+          {active ? "Aktywny" : "Nieaktywny"}
+          <div>
+            {`Rząd: ${position.row} Miejsce w rzędzie: ${position.number}`}
+          </div>
+        </li>
+      );
+    });
+  };
 
-  render() {
-    return <ul className='collection'>{this.renderBeehives()}</ul>;
-  }
-}
+  return <ul className='collection'>{renderBeehives(beehives)}</ul>;
+};
 
 BeehivesList.propTypes = {
   beehives: PropTypes.array.isRequired
